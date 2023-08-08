c59ed1ce4fb39d10c3f79224ca12b8148c3cc236
Bookwhale00 <Bookwhale00@gmail.com>
2023-08-08 15:58:49 +0900

    :memo: Feat: 08.08

---
+++ 2023.07/07.20/solution_1.py
@@ -0,0 +1,23 @@
+'''
+https://school.programmers.co.kr/learn/courses/30/lessons/172928
+공원 산책
+
+갈 수 있는 길 -> O
+장애물 -> X
+
+로봇 강아지에게 명령 ["방향 거리", "방향 거리"...]
+E 동 W 서 S 남 N 북
+
+로봇 강아지는 명령을 수행하기 전에
+1. 이동 중 공원을 벗어나는지
+2. 이동 중 장애물을 만나는지
+확인해서 해당하는 명령은 무시한다. 
+
+무슨말이지...??
+hint : BFS알고리즘
+'''
+
+def solution(park, routes):
+    answer = []
+    return answer
+
